e96ab56c1b23c1dc4c419b9abc5e045bd3700b1e
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-06-11 05:47:50 +0200

    Update PlayerList.tsx

--- src/components/PlayerList.tsx
+++ src/components/PlayerList.tsx
@@ -9,7 +9,7 @@ const PlayerList = (playerProps: PlayerProps) => {
     const playersSortedByScore = playerProps.players.sort((a, b) => b.score - a.score)
 
     return (
-        <div className="mt-30">
+        <div className="mt-10">
             {playersSortedByScore
                 .map(player =>
                     <Player playerData={player} key={player.id}
