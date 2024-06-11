8246452e45363111ddf916628a93eb7cfa008e40
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-06-11 05:48:12 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ScoreBoard from "@/components/ScoreBoard";
 
 export default function Home() {
     return (
-        <main className="flex flex-col items-center md:p-12 font-mono text-9xl">
+        <main className="flex flex-col items-center md:p-12 font-mono text-xl">
             <ScoreBoard/>
         </main>
     )
