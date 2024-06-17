b0db18e2302cda8f4af12f542593f7f93ffaf237
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-06-17 19:27:12 +0200

    Update Player.tsx

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -23,8 +23,8 @@ const Player = (playerProps: PlayerProps) => {
 
     return (
         <ul className="mt-2 flex justify-start border-solid border-2 border-white">
-            <li>{playerProps.playerData.name}</li>
-            <li className="ml-4">{playerScore}</li>
+            <li className="mx-4">{playerProps.playerData.name}</li>
+            <li>{playerScore}</li>
             <li className="ml-4"><ChangePlayerScore onChangePlayerScore={addPlayerScoreHandler}/></li>
             <li className="ml-4">
                 <button onClick={onSaveRemovePlayerHandler} type="button" className="bg-red-700">Remove
