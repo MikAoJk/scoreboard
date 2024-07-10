dcca3717961cd82ef6fb85d57d4d86e853e1d033
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-07-10 09:01:01 +0200

    Update AddPlayer.tsx

--- src/components/AddPlayer.tsx
+++ src/components/AddPlayer.tsx
@@ -27,7 +27,7 @@ const AddPlayer = (addPlayerProps: AddPlayerProps) => {
 
     return (
         <div>
-            {!isEditing && <button onClick={startEditingHandler} type="button" className="cursor-pointer bg-green-600 text-white rounded">Add</button>}
+            {!isEditing && <button onClick={startEditingHandler} type="button" className="cursor-pointer mt-10 bg-green-600 text-white rounded">Add</button>}
             {isEditing && <AddPlayerModal onSaveAddPlayer={saveAddPlayerHandler} onCancel={stopEditingHandler} numberOfPlayers={addPlayerProps.numberOfPlayers}/>}
         </div>
     )
