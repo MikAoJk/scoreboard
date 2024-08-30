2219e6b64f1065ab7e5a42adf915157243c337c4
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-08-30 18:43:25 +0200

    Update Player.tsx

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -24,7 +24,7 @@ const Player = (playerProps: PlayerProps) => {
     }
 
     return (
-        <ul className="mt-4 flex border-solid border-white border-2">
+        <ul className="mt-2 flex border-solid border-white border-2">
             <li className="w-1/4 text-left ml-2">{playerProps.playerData.name}</li>
             <li className="w-1/4">{playerProps.playerData.score}</li>
             <li className="w-1/4"><ChangePlayerScore onAddChangePlayerScore={addPlayerScoreHandler}
