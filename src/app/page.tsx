3fae69359538e3fd1458b0d4168c348f1fffd0b5
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-10-04 20:45:15 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ScoreBoard from "@/components/ScoreBoard";
 
 export default function Home() {
     return (
-        <main className="text:6xl">
+        <main className="text:7xl md:text:6xl">
             <ScoreBoard/>
         </main>
     );
