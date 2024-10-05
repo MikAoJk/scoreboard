e9ff5a74c4d0bf219a76e3b64525049c93113894
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-10-05 09:08:54 +0200

    Update Player.tsx

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -30,7 +30,7 @@ const Player = (playerProps: PlayerProps) => {
             <li className="w-1/4"><ChangePlayerScore onAddChangePlayerScore={addPlayerScoreHandler}
                                                     onSubstractChangePlayerScore={substractPlayerScoreHandler}/></li>
             <li className="w-1/4 text-right mr-2">
-                <button onClick={onSaveRemovePlayerHandler} type="button" className="bg-red-700 rounded font-bold py-2 px-4">Remove
+                <button onClick={onSaveRemovePlayerHandler} type="button" className="bg-red-700 rounded font-bold py-2 px-2">Remove
                 </button>
             </li>
         </ul>
