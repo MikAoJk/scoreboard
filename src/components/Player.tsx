a6ae642151b2f299aaf3d1794282499cda8dfdec
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-10-05 08:38:51 +0200

    Update Player.tsx

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -26,7 +26,7 @@ const Player = (playerProps: PlayerProps) => {
     return (
         <ul className="mt-2 flex border-solid border-white border-2">
             <li className="w-1/4 text-left ml-2">{playerProps.playerData.name}</li>
-            <li className="w-1/4">{playerProps.playerData.score}</li>
+            <li className="w-1/6">{playerProps.playerData.score}</li>
             <li className="w-1/4"><ChangePlayerScore onAddChangePlayerScore={addPlayerScoreHandler}
                                                     onSubstractChangePlayerScore={substractPlayerScoreHandler}/></li>
             <li className="w-1/4 text-right mr-2">
