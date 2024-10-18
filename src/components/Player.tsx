31f9ea473873ff79321bafc2d98e8094c3a4fb78
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2024-10-18 09:56:26 +0200

    Small ui fix

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -25,11 +25,11 @@ const Player = (playerProps: PlayerProps) => {
 
     return (
         <ul className="mt-2 flex border-solid border-white border-2">
-            <li className="w-1/5 text-left ml-2">{playerProps.playerData.name}</li>
+            <li className="w-1/6 text-left ml-2">{playerProps.playerData.name}</li>
             <li className="w-1/5">{playerProps.playerData.score}</li>
-            <li className="w-1/4"><ChangePlayerScore onAddChangePlayerScore={addPlayerScoreHandler}
+            <li className="w-1/3"><ChangePlayerScore onAddChangePlayerScore={addPlayerScoreHandler}
                                                     onSubstractChangePlayerScore={substractPlayerScoreHandler}/></li>
-            <li className="w-1/4 text-right">
+            <li className="w-1/3 text-right mr-2">
                 <button onClick={onSaveRemovePlayerHandler} type="button" className="bg-red-700 rounded font-bold py-2 px-2">Remove
                 </button>
             </li>
